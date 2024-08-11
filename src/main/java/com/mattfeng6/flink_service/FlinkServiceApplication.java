62d6ed3b85739657ab4173802b12f4f6b12f9024
mattfeng6 <matthewfznn@gmail.com>
2024-08-11 00:15:48 -0400

    initial commit

---
+++ src/main/java/com/mattfeng6/flink_service/FlinkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mattfeng6.flink_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlinkServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mattfeng6/flink_service/FlinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattfeng6.flink_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
